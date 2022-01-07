72f35c098d7da7c9d575eaa6f56ebcf7ee4a58bd
Omkar <omkargurav1920@gmail.com>
2022-01-07 22:41:59 +0530

    third commit

--- assignments/palindromM2.java
+++ assignments/palindromM2.java
@@ -2,6 +2,16 @@ package com.omkar.assignments;
 
 public class palindromM2 {
     public static void main(String[] args) {
-        
+        String str = "abba";
+        String rev = " ";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+
+        }
+    if (str.equals(rev)){
+        System.out.println("Palindrome");
+    }
+    else System.out.println("Not a Palindrome");
     }
 }
